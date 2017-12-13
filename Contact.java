3ab053a4bc635f3274fce2411741add96bcb4cb3
joshuacassidy <Joshcassidy79@gmail.com>
2017-12-13 17:13:47 +0000

    initial commit

---
+++ Contact.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phonebook;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Josh
+ */
+public class Contact implements Serializable {
+    private String name, address, phoneNumber;
+
+    public Contact(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    
+    
+}
---
+++ Phonebook.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phonebook;
+
+/**
+ *
+ * @author Josh
+ */
+public class Phonebook {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PhonebookGUI phonebookGUI = new PhonebookGUI();
+        phonebookGUI.setVisible(true);
+    }
+    
+}
